0de5f53ab3974deddfb96400feb16bc610ad0d72
d_malov <d_malov@yahoo.com>
2018-04-19 11:23:33 +0300

    Logger classes added

---
+++ src/main/java/com.db.edu.logger/LoggerSaver.java
@@ -0,0 +1,10 @@
+package com.db.edu.logger;
+
+/**
+ * Created by Java_1 on 19.04.2018.
+ */
+public class LoggerSaver {
+    public void save(String message) {
+        System.out.println();
+    }
+}
